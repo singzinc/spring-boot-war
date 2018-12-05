f4de3cbdad3baed1639a1623a1e38079feca4df1
singzinc <sing.zinc.hk@gmail.com>
2018-12-06 00:35:42 +0800

    first commit

---
+++ spring-boot-war/src/main/java/com/singplayground/Application.java
@@ -0,0 +1,23 @@
+package com.singplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+
+	 @Override
+	 protected SpringApplicationBuilder configure( SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	 }
+	 
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	
+}
---
+++ spring-boot-war/src/main/java/com/singplayground/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author 
+ *
+ */
+package com.singplayground;
