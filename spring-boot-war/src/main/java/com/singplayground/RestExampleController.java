9c0c14c3e437546c87b34aa1e0b111171b2bafa1
singzinc <sing.zinc.hk@gmail.com>
2019-01-13 22:00:12 +0800

    insert the properties

--- spring-boot-war/src/main/java/com/singplayground/RestExampleController.java
+++ spring-boot-war/src/main/java/com/singplayground/RestExampleController.java
@@ -4,18 +4,29 @@ package com.singplayground;
 
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@PropertySource("classpath:ui.properties")
+
+
+
 @RestController
 public class RestExampleController {
 
+	
+	@Value("${test.ui}")
+	private String ui;
+	
+	
 	@RequestMapping("example1")
 	String home() {
-		return "Hello World! this is 2";
+		return "Hello World! this is 2" + ui;
 	}
 
 	@RequestMapping(value = "example2", method = RequestMethod.GET)
